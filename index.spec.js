02c9948b0ba8c5e65d8b994a5324bb9390805732
Walmyr <wlsf82@gmail.com>
2022-01-29 02:12:36 +0100

    Add another failing test for another acceptance criteria
    Now the failure is:
    Expected: 2200
    Received: 200

--- index.spec.js
+++ index.spec.js
@@ -17,3 +17,26 @@ test('lista com um produto retorna preco * quantidade', () => {
 
   expect(resultadoAtual).toBe(resultadoEsperado)
 })
+
+test('lista com mais de um produto retorna soma de (preco * quantidade) de cada produto', () => {
+  const resultadoAtual = calculaPrecoFinal([
+    {
+      id: 1,
+      preco: 200,
+      quantidade: 1
+    },
+    {
+      id: 2,
+      preco: 400,
+      quantidade: 2
+    },
+    {
+      id: 3,
+      preco: 400,
+      quantidade: 3
+    },
+  ])
+  const resultadoEsperado = 2200
+
+  expect(resultadoAtual).toBe(resultadoEsperado)
+})

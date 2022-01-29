ef5c10e26344369d6ca20543569133aad66dbaf8
Walmyr <wlsf82@gmail.com>
2022-01-29 02:36:04 +0100

    Make all tests pass

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = function(listaDeProdutos) {
     .reduce((a, b) => a + b, 0)
 
   if (precoSemiFinal < 100) return precoSemiFinal
-  if (precoSemiFinal >= 100) return precoSemiFinal * .95
+  if (precoSemiFinal >= 100 && precoSemiFinal < 250) return precoSemiFinal * .95
+  if (precoSemiFinal >= 250) return precoSemiFinal * .9
 }
 
 function calculaPrecoProduto(produto) {
